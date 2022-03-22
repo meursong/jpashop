2684a48127dfffd866197f82105b7bbb7086b92c
meursong <94438960+meursong@users.noreply.github.com>
2022-03-22 16:28:28 +0900

    project setting

---
+++ src/main/java/jpashop/jpashop/HelloController.java
@@ -0,0 +1,16 @@
+package jpashop.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){ //데이터를 심어서 view에 넘기는 model
+        model.addAttribute("data","hello!!!");
+        return "hello"; //return은 화면 이름 (.html생략)
+    }
+
+}
---
+++ src/main/java/jpashop/jpashop/hello.java
@@ -0,0 +1,10 @@
+package jpashop.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class hello {
+
+    private String data;
+}
