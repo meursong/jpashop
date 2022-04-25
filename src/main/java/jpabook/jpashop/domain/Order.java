cbc3ce95323974a63cd5266013988268693513b1
meursong <94438960+meursong@users.noreply.github.com>
2022-04-25 23:28:47 +0900

    order/orderitem business logic added

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -83,4 +83,19 @@ public class Order {
             orderItem.cancel();
         }
     }
+    //== 조회 로직 ==//
+    /*
+     * 전체 주문 가격 조회
+     * */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+
+        /*return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();*/
+    }
 }
